87db52e19b6c0d42243766ada5e96489888302e3
Ramesh Marikhu <marikhu@gmail.com>
2023-11-28 10:25:56 +0545

    Added script to convert from .pt to .onnx using export_yoloV8.py from https://github.com/marcoslucianops/DeepStream-Yolo.git

---
+++ models/convert-pt-to-onnx-ultralytics.sh
@@ -0,0 +1,7 @@
+#~/works/DeepStream-Yolo/utils/export_yoloV8.py
+
+#python export_yoloV8.py --weights yolov8n.pt --dynamic
+python export_yoloV8.py --weights yolov8s.pt --dynamic
+python export_yoloV8.py --weights yolov8m.pt --dynamic
+#python export_yoloV8.py --weights yolov8l.pt --dynamic
+#python export_yoloV8.py --weights yolov8x.pt --dynamic
